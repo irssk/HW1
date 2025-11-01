5a5f02cc3e68eee2d7223c62537d7f12779fe118
Ira <ira350520@gmail.com>
2025-11-01 09:40:20 +0200

    Add Calculator class

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Calculator
+{
+    public class Calculator
+    {
+        public double LastResult { get; protected set; }
+
+        public double Add(double a, double b)
+        {
+            LastResult = a + b;
+            return LastResult;
+        }
+
+        public double Subtract(double a, double b)
+        {
+            LastResult = a - b;
+            return LastResult;
+        }
+
+        public double Multiply(double a, double b)
+        {
+            LastResult = a * b;
+            return LastResult;
+        }
+
+        public virtual double Divide(double a, double b)
+        {
+            if (b == 0)
+                throw new DivideByZeroException("Помилка: поділ на нуль неможливий.");
+            LastResult = a / b;
+            return LastResult;
+        }
+    }
+}
